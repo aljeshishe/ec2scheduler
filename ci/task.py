88e1f5ff56bd8d7ba3f35223aa8a3923dd2ea20e
Your Name <you@example.com>
2023-02-27 11:21:39 +0200

    added creating bucket for ci

--- ci/task.py
+++ ci/task.py
@@ -1,23 +1,7 @@
-print("hello")
-def run():
-    import boto3
+import boto3
 
-    # Create a SimpleDB client
-    sdb = boto3.client('sdb')
+bucket_name = "ec2scheduler-complete"
+print(f"Creating {bucket_name=}")
 
-    # Create a domain
-    sdb.create_domain(DomainName='ec2scheduler')
-
-    # Put some data into the domain
-    sdb.put_attributes(DomainName='ec2scheduler', ItemName='result', Attributes=[
-        {'Name': 'attribute1', 'Value': 'value1'},
-        {'Name': 'attribute2', 'Value': 'value2'},
-    ])
-
-    # Query the data
-    response = sdb.select(SelectExpression='select * from mydomain')
-    items = response['Items']
-    for item in items:
-        print(item['Name'])
-        for attribute in item['Attributes']:
-            print(attribute['Name'], attribute['Value'])
+s3 = boto3.resource('s3')
+s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': 'eu-central-1'})
---
+++ ci/wait_ci.py
@@ -0,0 +1,32 @@
+import time
+
+import boto3
+
+expected_entity = "ec2scheduler-complete"
+
+
+def run(timeout=60*3):
+    print(f"Waiting for {expected_entity} bucket...")
+    s3 = boto3.resource('s3')
+    start = time.time()
+    while time.time() - start < timeout:
+        result = s3.buckets.all()
+        for bucket in result:
+            if bucket.name == expected_entity:
+                print(f"Found {expected_entity}. Deleting")
+                bucket.delete()
+                exit(0)
+    print(f"{expected_entity} not found")
+    exit(1)
+
+
+if __name__ == "__main__":
+    run()
+
+# sdb.list_domains() results example
+# {
+#     'DomainNames': [
+#         'string',
+#     ],
+#     'NextToken': 'string'
+# }
